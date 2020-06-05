28610d82a4a34a951b097edac20952f324bc5d7e
Joost Aafjes <joostaafjes@gmail.com>
2020-06-05 19:05:57 +0200

    add plot adapt for udemy

--- udemy-challenge/capacity-constraints-or-tool.py
+++ udemy-challenge/capacity-constraints-or-tool.py
@@ -4,83 +4,32 @@ from __future__ import print_function
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
+from timeit import default_timer as timer
 
-def create_data_model():
+import numpy as np
+import scipy.spatial.distance as ssd
+
+import plotly.graph_objects as go
+
+
+def calculate_distances(X_Coordinations, Y_Coordinations):
+    # Stack them on top of each other
+    X_Y_Coordinations = np.column_stack((X_Coordinations, Y_Coordinations))
+
+    # Create a distance matrix from the Euclidean distance
+    Distance_Matrix = ssd.cdist(X_Y_Coordinations, X_Y_Coordinations, 'euclidean')
+
+    dist_matrix = Distance_Matrix.astype(int).tolist()
+    return dist_matrix
+
+
+def create_data_model(X_Coordinations, Y_Coordinations):
     """Stores the data for the problem."""
     data = {}
-    data['distance_matrix'] = [
-        [
-            0, 548, 776, 696, 582, 274, 502, 194, 308, 194, 536, 502, 388, 354,
-            468, 776, 662
-        ],
-        [
-            548, 0, 684, 308, 194, 502, 730, 354, 696, 742, 1084, 594, 480, 674,
-            1016, 868, 1210
-        ],
-        [
-            776, 684, 0, 992, 878, 502, 274, 810, 468, 742, 400, 1278, 1164,
-            1130, 788, 1552, 754
-        ],
-        [
-            696, 308, 992, 0, 114, 650, 878, 502, 844, 890, 1232, 514, 628, 822,
-            1164, 560, 1358
-        ],
-        [
-            582, 194, 878, 114, 0, 536, 764, 388, 730, 776, 1118, 400, 514, 708,
-            1050, 674, 1244
-        ],
-        [
-            274, 502, 502, 650, 536, 0, 228, 308, 194, 240, 582, 776, 662, 628,
-            514, 1050, 708
-        ],
-        [
-            502, 730, 274, 878, 764, 228, 0, 536, 194, 468, 354, 1004, 890, 856,
-            514, 1278, 480
-        ],
-        [
-            194, 354, 810, 502, 388, 308, 536, 0, 342, 388, 730, 468, 354, 320,
-            662, 742, 856
-        ],
-        [
-            308, 696, 468, 844, 730, 194, 194, 342, 0, 274, 388, 810, 696, 662,
-            320, 1084, 514
-        ],
-        [
-            194, 742, 742, 890, 776, 240, 468, 388, 274, 0, 342, 536, 422, 388,
-            274, 810, 468
-        ],
-        [
-            536, 1084, 400, 1232, 1118, 582, 354, 730, 388, 342, 0, 878, 764,
-            730, 388, 1152, 354
-        ],
-        [
-            502, 594, 1278, 514, 400, 776, 1004, 468, 810, 536, 878, 0, 114,
-            308, 650, 274, 844
-        ],
-        [
-            388, 480, 1164, 628, 514, 662, 890, 354, 696, 422, 764, 114, 0, 194,
-            536, 388, 730
-        ],
-        [
-            354, 674, 1130, 822, 708, 628, 856, 320, 662, 388, 730, 308, 194, 0,
-            342, 422, 536
-        ],
-        [
-            468, 1016, 788, 1164, 1050, 514, 514, 662, 320, 274, 388, 650, 536,
-            342, 0, 764, 194
-        ],
-        [
-            776, 868, 1552, 560, 674, 1050, 1278, 742, 1084, 810, 1152, 274,
-            388, 422, 764, 0, 798
-        ],
-        [
-            662, 1210, 754, 1358, 1244, 708, 480, 856, 514, 468, 354, 844, 730,
-            536, 194, 798, 0
-        ],
-    ]
-    data['demands'] = [0, 1, 1, 2, 4, 2, 4, 8, 8, 1, 2, 1, 2, 4, 4, 8, 8]
-    data['vehicle_capacities'] = [15, 15, 15, 15]
-    data['num_vehicles'] = 4
+    data['distance_matrix'] = calculate_distances(X_Coordinations, Y_Coordinations)
+    data['demands'] = [0, 19, 21, 6, 19, 7, 12, 16, 6, 16, 8, 14, 21, 16, 3, 22, 18, 19, 1, 24, 8, 12, 4, 8, 24, 24, 2, 20, 15, 2, 14, 9]
+    data['vehicle_capacities'] = [100, 100, 100, 100, 100]
+    data['num_vehicles'] = 5
     data['depot'] = 0
     return data
 
@@ -112,11 +61,46 @@ def print_solution(data, manager, routing, solution):
     print('Total distance of all routes: {}m'.format(total_distance))
     print('Total load of all routes: {}'.format(total_load))
 
+def plot_solution(data, manager, routing, solution, X_Coordinations, Y_Coordinations):
+    """Plot solution."""
+    fig = go.Figure()
+    for vehicle_id in range(data['num_vehicles']):
+        index = routing.Start(vehicle_id)
+        plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+        route_distance = 0
+        route_load = 0
+        x = []
+        y = []
+        while not routing.IsEnd(index):
+            node_index = manager.IndexToNode(index)
+            x.append(X_Coordinations[node_index])
+            y.append(Y_Coordinations[node_index])
+            route_load += data['demands'][node_index]
+            plan_output += ' {0} Load({1}) -> '.format(node_index, route_load)
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            # x.append(X_Coordinations[index])
+            # y.append(Y_Coordinations[index])
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id)
+        node_index = manager.IndexToNode(index)
+        x.append(X_Coordinations[node_index])
+        y.append(Y_Coordinations[node_index])
+        fig.add_trace(go.Scatter(x=x, y=y,
+                                 mode='lines+markers',
+                                 name=f'route for {vehicle_id}'))
+
+    fig.show()
 
 def main():
+    start = timer()
+
+    X_Coordinations = np.array([82, 96, 50, 49, 13, 29, 58, 84, 14, 2, 3, 5, 98, 84, 61, 1, 88, 91, 19, 93, 50, 98, 5, 42, 61, 9, 80, 57, 23, 20, 85, 98])
+    Y_Coordinations = np.array([76, 44, 5, 8, 7, 89, 30, 39, 24, 39, 82, 10, 52, 25, 59, 65, 51, 2, 32, 3, 93, 14, 42, 9, 62, 97, 55, 69, 15, 70, 60, 5])
+
     """Solve the CVRP problem."""
     # Instantiate the data problem.
-    data = create_data_model()
+    data = create_data_model(X_Coordinations, Y_Coordinations)
 
     # Create the routing index manager.
     manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
@@ -167,7 +151,10 @@ def main():
     # Print solution on console.
     if solution:
         print_solution(data, manager, routing, solution)
+        plot_solution(data, manager, routing, solution, X_Coordinations, Y_Coordinations)
 
+    end = timer()
+    print(end - start) # Time in seconds
 
 if __name__ == '__main__':
     main()
