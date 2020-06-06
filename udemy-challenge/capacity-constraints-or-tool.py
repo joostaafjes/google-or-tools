07a9dde0c57b661f4bfe2cb2b39885730ca84852
Joost Aafjes <joostaafjes@gmail.com>
2020-06-06 15:11:57 +0200

    add search strategy

--- udemy-challenge/capacity-constraints-or-tool.py
+++ udemy-challenge/capacity-constraints-or-tool.py
@@ -135,6 +135,19 @@ def main():
     search_parameters.first_solution_strategy = (
         routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
 
+    # Setting search strategy
+    # AUTOMATIC Lets the solver select the metaheuristic.
+    # GREEDY_DESCENT Accepts improving(cost - reducing) local search neighbors until a local minimum is reached.
+    # GUIDED_LOCAL_SEARCH Uses guided local search to escape local minima(cf.http: // en.wikipedia.org / wiki / Guided_Local_Search); this is generally the most efficient metaheuristic ->
+    # SIMULATED_ANNEALING Uses simulated annealing to escape local minima(cf.http: // en.wikipedia.org / wiki / Simulated_annealing).
+    # TABU_SEARCH Uses tabu search to escape local minima(cf.http: // en.wikipedia.org / wiki / Tabu_search).
+    # OBJECTIVE_TABU_SEARCH Uses tabu search on the objective value of solution to escape local minima
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.local_search_metaheuristic = (
+    routing_enums_pb2.LocalSearchMetaheuristic.TABU_SEARCH)
+    search_parameters.time_limit.seconds = 30
+    search_parameters.log_search = True
+
     # Solve the problem.
     solution = routing.SolveWithParameters(search_parameters)
 
