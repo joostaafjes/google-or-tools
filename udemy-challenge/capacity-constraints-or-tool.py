8d4a13a4c0f887439c7f3ac0aa7e34ae8215c0a7
Joost Aafjes <joostaafjes@gmail.com>
2020-06-12 10:32:27 +0200

    automate different strategies/times

--- udemy-challenge/capacity-constraints-or-tool.py
+++ udemy-challenge/capacity-constraints-or-tool.py
@@ -34,7 +34,7 @@ def create_data_model(X_Coordinations, Y_Coordinations):
     return data
 
 
-def print_solution(data, manager, routing, solution):
+def print_solution(data, manager, routing, solution, took_time, filename):
     """Prints solution on console."""
     total_distance = 0
     total_load = 0
@@ -58,8 +58,14 @@ def print_solution(data, manager, routing, solution):
         print(plan_output)
         total_distance += route_distance
         total_load += route_load
-    print('Total distance of all routes: {}m'.format(total_distance))
-    print('Total load of all routes: {}'.format(total_load))
+    line = (f'Total distance of all routes: {total_distance}m\n'
+            f'Total load of all routes: {total_load}\n'
+            f'Total time {took_time} sec')
+    file = open(filename, "w+")
+    file.write(line)
+    file.close()
+    print(line)
+
 
 def plot_solution(data, manager, routing, solution, X_Coordinations, Y_Coordinations):
     """Plot solution."""
@@ -83,81 +89,93 @@ def plot_solution(data, manager, routing, solution, X_Coordinations, Y_Coordinat
     fig.show()
 
 def main():
-    start = timer()
 
-    X_Coordinations = np.array([82, 96, 50, 49, 13, 29, 58, 84, 14, 2, 3, 5, 98, 84, 61, 1, 88, 91, 19, 93, 50, 98, 5, 42, 61, 9, 80, 57, 23, 20, 85, 98])
-    Y_Coordinations = np.array([76, 44, 5, 8, 7, 89, 30, 39, 24, 39, 82, 10, 52, 25, 59, 65, 51, 2, 32, 3, 93, 14, 42, 9, 62, 97, 55, 69, 15, 70, 60, 5])
-
-    """Solve the CVRP problem."""
-    # Instantiate the data problem.
-    data = create_data_model(X_Coordinations, Y_Coordinations)
-
-    # Create the routing index manager.
-    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
-                                           data['num_vehicles'], data['depot'])
-
-    # Create Routing Model.
-    routing = pywrapcp.RoutingModel(manager)
-
-
-    # Create and register a transit callback.
-    def distance_callback(from_index, to_index):
-        """Returns the distance between the two nodes."""
-        # Convert from routing variable Index to distance matrix NodeIndex.
-        from_node = manager.IndexToNode(from_index)
-        to_node = manager.IndexToNode(to_index)
-        return data['distance_matrix'][from_node][to_node]
-
-    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
-
-    # Define cost of each arc.
-    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
-
-
-    # Add Capacity constraint.
-    def demand_callback(from_index):
-        """Returns the demand of the node."""
-        # Convert from routing variable Index to demands NodeIndex.
-        from_node = manager.IndexToNode(from_index)
-        return data['demands'][from_node]
-
-    demand_callback_index = routing.RegisterUnaryTransitCallback(
-        demand_callback)
-    routing.AddDimensionWithVehicleCapacity(
-        demand_callback_index,
-        0,  # null capacity slack
-        data['vehicle_capacities'],  # vehicle maximum capacities
-        True,  # start cumul to zero
-        'Capacity')
-
-    # Setting first solution heuristic.
-    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-
-    # Setting search strategy
     # AUTOMATIC Lets the solver select the metaheuristic.
     # GREEDY_DESCENT Accepts improving(cost - reducing) local search neighbors until a local minimum is reached.
-    # GUIDED_LOCAL_SEARCH Uses guided local search to escape local minima(cf.http: // en.wikipedia.org / wiki / Guided_Local_Search); this is generally the most efficient metaheuristic ->
+    # GUIDED_LOCAL_SEARCH Uses guided local search to escape local minima(cf.http: // en.wikipedia.org / wiki / Guided_Local_Search); this is generally the most efficient metaheuristic
     # SIMULATED_ANNEALING Uses simulated annealing to escape local minima(cf.http: // en.wikipedia.org / wiki / Simulated_annealing).
     # TABU_SEARCH Uses tabu search to escape local minima(cf.http: // en.wikipedia.org / wiki / Tabu_search).
-    # OBJECTIVE_TABU_SEARCH Uses tabu search on the objective value of solution to escape local minima
-    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.local_search_metaheuristic = (
-    routing_enums_pb2.LocalSearchMetaheuristic.TABU_SEARCH)
-    search_parameters.time_limit.seconds = 30
-    search_parameters.log_search = True
+    # OBJECTIVE_TABU_SEARCH Uses tabu search on the objective value of solution to escape local minima (Note: not working)
+    for search_strategy_name, search_strategy in [
+        ["AUTOMATIC", routing_enums_pb2.LocalSearchMetaheuristic.AUTOMATIC],
+        ["GREEDY_DESCENT", routing_enums_pb2.LocalSearchMetaheuristic.GREEDY_DESCENT],
+        ["GUIDED_LOCAL_SEARCH", routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH],
+        ["SIMULATED_ANNEALING", routing_enums_pb2.LocalSearchMetaheuristic.SIMULATED_ANNEALING],
+        ["TABU_SEARCH", routing_enums_pb2.LocalSearchMetaheuristic.TABU_SEARCH],
+    ]:
+        for time_limit in range(1, 7):
+            start = timer()
 
-    # Solve the problem.
-    solution = routing.SolveWithParameters(search_parameters)
+            X_Coordinations = np.array([82, 96, 50, 49, 13, 29, 58, 84, 14, 2, 3, 5, 98, 84, 61, 1, 88, 91, 19, 93, 50, 98, 5, 42, 61, 9, 80, 57, 23, 20, 85, 98])
+            Y_Coordinations = np.array([76, 44, 5, 8, 7, 89, 30, 39, 24, 39, 82, 10, 52, 25, 59, 65, 51, 2, 32, 3, 93, 14, 42, 9, 62, 97, 55, 69, 15, 70, 60, 5])
 
-    # Print solution on console.
-    if solution:
-        print_solution(data, manager, routing, solution)
-        plot_solution(data, manager, routing, solution, X_Coordinations, Y_Coordinations)
+            """Solve the CVRP problem."""
+            # Instantiate the data problem.
+            data = create_data_model(X_Coordinations, Y_Coordinations)
 
-    end = timer()
-    print(end - start) # Time in seconds
+            # Create the routing index manager.
+            manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
+                                                   data['num_vehicles'], data['depot'])
+
+            # Create Routing Model.
+            routing = pywrapcp.RoutingModel(manager)
+
+
+            # Create and register a transit callback.
+            def distance_callback(from_index, to_index):
+                """Returns the distance between the two nodes."""
+                # Convert from routing variable Index to distance matrix NodeIndex.
+                from_node = manager.IndexToNode(from_index)
+                to_node = manager.IndexToNode(to_index)
+                return data['distance_matrix'][from_node][to_node]
+
+            transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+            # Define cost of each arc.
+            routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+
+            # Add Capacity constraint.
+            def demand_callback(from_index):
+                """Returns the demand of the node."""
+                # Convert from routing variable Index to demands NodeIndex.
+                from_node = manager.IndexToNode(from_index)
+                return data['demands'][from_node]
+
+            demand_callback_index = routing.RegisterUnaryTransitCallback(
+                demand_callback)
+            routing.AddDimensionWithVehicleCapacity(
+                demand_callback_index,
+                0,  # null capacity slack
+                data['vehicle_capacities'],  # vehicle maximum capacities
+                True,  # start cumul to zero
+                'Capacity')
+
+            # Setting first solution heuristic.
+            search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+            search_parameters.first_solution_strategy = (
+                routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+            # Setting search strategy
+            # AUTOMATIC Lets the solver select the metaheuristic.
+            # GREEDY_DESCENT Accepts improving(cost - reducing) local search neighbors until a local minimum is reached.
+            # GUIDED_LOCAL_SEARCH Uses guided local search to escape local minima(cf.http: // en.wikipedia.org / wiki / Guided_Local_Search); this is generally the most efficient metaheuristic ->
+            # SIMULATED_ANNEALING Uses simulated annealing to escape local minima(cf.http: // en.wikipedia.org / wiki / Simulated_annealing).
+            # TABU_SEARCH Uses tabu search to escape local minima(cf.http: // en.wikipedia.org / wiki / Tabu_search).
+            # OBJECTIVE_TABU_SEARCH Uses tabu search on the objective value of solution to escape local minima
+            search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+            search_parameters.local_search_metaheuristic = (search_strategy)
+            search_parameters.time_limit.seconds = time_limit
+            search_parameters.log_search = True
+
+            # Solve the problem.
+            solution = routing.SolveWithParameters(search_parameters)
+
+            end = timer()
+            # Print solution on console.
+            if solution:
+                print_solution(data, manager, routing, solution, end - start, f'./results/{search_strategy_name}-timelimit{time_limit:02d}-sec.txt')
+                # plot_solution(data, manager, routing, solution, X_Coordinations, Y_Coordinations)
 
 if __name__ == '__main__':
     main()
